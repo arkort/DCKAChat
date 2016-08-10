e1943ef9af7feebb401080731c842ef1d1f5f50c
Artem_Koroteev <artem_koroteev@epam.com>
2016-08-10 18:57:09 +0300

    Add simple command mechanism

--- server.js
+++ server.js
@@ -9,15 +9,43 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-    console.log('a user ' + socket.id +' connected');
+    var nick = socket.id;
+
+    console.log('user ' + nick + ' connected');
+
+    io.emit('chat message', { message: nick + ' has joined', isControl: true, for: 'everyone' });
 
     socket.on('chat message', function (msg) {
-        socket.broadcast.emit('chat message', { message: msg, sender: socket.id });
+        process(msg);
     });
 
     socket.on('disconnect', function () {
-        console.log('user disconnected');
+        console.log('user ' + nick + ' disconnected');
+        io.emit('chat message', { message: 'user ' + nick + ' disconnected', isControl: true, for: 'everyone' });
     });
+
+    function process(msg) {
+        if (msg[0] == '/') {
+            var words = msg.split(' ');
+
+            switch (words[0]) {
+                case '/?':
+                    socket.emit('chat message', { message: '/nick - change your nick', isControl: true });
+                    break;
+                case '/nick':
+                    var oldNick = nick;
+                    nick = words[1];
+                    io.emit('chat message',  { message: oldNick+' changed name to '+nick, sender: nick, isControl: true, for: 'everyone' });
+                    break;
+                default:
+                    io.emit('chat message', { message: msg, sender: nick, isControl: false, for: 'everyone' });
+                    break;
+            }
+        }
+        else {
+            io.emit('chat message', { message: msg, sender: nick, isControl: false, for: 'everyone' });
+        }
+    }
 });
 
 http.listen(port, function () {
